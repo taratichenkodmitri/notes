fe66c605b1f13725107e4dfee46319bfad0e670d
taratichenkodmitri <tarataichenkodmitri@gmail.com>
2024-06-21 19:02:03 +0500

    [+] add props to note

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import './App.css';
 import Button from './components/Button/Button';
+import Note from './components/Note/Note';
 
 function App() {
-  return <Button></Button>;
+  const notes = [
+    {
+      title: 'Подготовка к обновлению курсов',
+      text: 'Сегодня провёл весь день за...',
+      date: new Date(),
+    },
+    {
+      title: 'Поход в годы',
+      text: 'Думал, что очень много време...',
+      date: new Date(),
+    },
+  ];
+  return (
+    <>
+      <Note
+        title={notes[0].title}
+        text={notes[0].text}
+        date={notes[0].date}
+      />
+      <Note
+        title={notes[1].title}
+        text={notes[1].text}
+        date={notes[1].date}
+      />
+      <Button></Button>
+    </>
+  );
 }
 
 export default App;
--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,11 +1,13 @@
 import './Note.css';
 
 const Note = ({ title, date, text }) => {
+  const formattedDate = new Intl.DateTimeFormat('ru-Ru').format(date);
+
   return (
     <div className="note">
       <div className="note__header">{title}</div>
       <div className="note__body">
-        <div className="note__date">{date}</div>
+        <div className="note__date">{formattedDate}</div>
         <div className="note__text">{text}</div>
       </div>
     </div>
